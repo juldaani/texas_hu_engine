83eea5a49b28eec2d4d91cdc3be1f1136ef02cfd
juldaani <juho.uusiluomalahti@gmail.com>
2018-12-15 17:43:34 +0200

    refactoring in progress

--- engine.py
+++ engine.py
@@ -9,58 +9,110 @@ Created on Fri Dec  7 22:26:05 2018
 import numpy as np
 
 
-# %%
 
-# "Static" variables (over single round)
-tmpCards = np.random.choice(52, size=9, replace=0)
-boardCards = tmpCards[:5]
-holeCards = np.array([tmpCards[5:7],tmpCards[7:]])
-blindPositions = np.random.choice(2, size=2, replace=0)     # 1st index = small blind player
-smallBlindAmount = 4
-#bigBlindAmount = smallBlindAmount*2
-
-# "Nonstatic" variables, these are modified by the engine
-stacks = np.array([smallBlindAmount*8+2,smallBlindAmount*6+5])
-#bets = np.array([0,0])
-#hasPlayerActed = np.array([0,0])
-#pot = -1
-#playerTurn = -1
-#gameStartState = 1
-#gameEndState = 0
+# board: pot, smallBlindAmount, bigBlindAmount, boardCardsVisible, boardCards
+# players: holeCards, stacks, bets, isSmallBlind, isBigBlind, isPlayersTurn, hasActed
+# availableActions: fold, callAmount, raiseMinMax
+# controlVariables: bettingRound, gameEndState
 
 
+# Player state ................................................................
+def getStacks(playerStates): return playerStates[:,2]
 
-# %%
+def getBets(playerStates): return playerStates[:,3]
 
-def initGame(blindPositions, smallBlindAmount, stacks):
-    pot = 0
-    bets = np.array([0,0])
-    numPlayersActed = 0
-    gameEndState = 0
-    bettingRound = 0    # 0 = pre-flop, 1 = flop, 2 = turn, 3 = river
-    boardCardsVisible = np.zeros(5, dtype=np.int)-1
-    bigBlindAmount = smallBlindAmount*2
+def setBet(playerStates, amount, playerIdx):
+    playerStates[playerIdx,2] -= amount
+    playerStates[playerIdx,3] += amount
+    return playerStates
+
+def setActingPlayer(playerStates, playerIdx): 
+    otherPlayerIdx = np.abs(playerIdx-1)
+    playerStates[playerIdx,6] = 1
+    playerStates[otherPlayerIdx,6] = 0
+    return playerStates
+
+# Board state .................................................................
+def getSmallBlindAmount(boardState): return boardState[1]
+
+def getBigBlindAmount(boardState): return boardState[2]
+
+
+
+def initGame(boardCards, smallBlindPlayerIdx, smallBlindAmount, stacks, holeCards):
+    
+    # Set board
+    board = np.zeros(13, dtype=np.int)
+    board[0] = 0                    # Pot
+    board[1] = smallBlindAmount     # Small blind
+    board[2] = smallBlindAmount*2   # Big blind
+    board[3:8] = 0                  # Visible board cards
+    board[8:] = boardCards          # Board cards
+    
+    # Set players
+    players = np.zeros((2,8), dtype=np.int)
+    players[:,:2] = holeCards       # Hole cards
+    players[:,2] = stacks           # Stacks
+    players[:,3] = 0                # Bets
+    players[smallBlindPlayerIdx,4] = 1      # Set small blind for the player
+    bigBlindPlayerIdx = np.abs(smallBlindPlayerIdx-1)
+    players[bigBlindPlayerIdx,5] = 1        # Set big blind for the player
+    players = setActingPlayer(players, smallBlindPlayerIdx)  # Small blind acts first on the preflop in heads up
+    players[:,7] = 0                # Has player acted?
+    
+    # Set game control variables
+    controlVariables = np.zeros(3, dtype=np.int)
+    controlVariables[0] = 0         # Betting round
+    controlVariables[1] = 0         # Game end state
+    
+#    pot = 0
+#    bets = np.array([0,0])
+#    numPlayersActed = 0
+#    gameEndState = 0
+#    bettingRound = 0    # 0 = pre-flop, 1 = flop, 2 = turn, 3 = river
+#    boardCardsVisible = np.zeros(5, dtype=np.int)-1
+#    bigBlindAmount = smallBlindAmount*2
     
     # Check that both players have enough money
-    if(not np.all(stacks >= bigBlindAmount)):
-        print('ERROR')
-        return None
-        # TODO: return error
+    if(not np.all(getStacks(players) >= getBigBlindAmount(board))):
+        print('ERROR, not enough money')
+        board = None
+        players = None
+        availableActions = None
+        controlVariables = None
+        return board, players, availableActions, controlVariables
     
-    smallBlindPlayerIdx = blindPositions[0]
-    bigBlindPlayerIdx = blindPositions[1]
+#    smallBlindPlayerIdx = blindPositions[0]
+#    bigBlindPlayerIdx = blindPositions[1]
     
-    stacks[smallBlindPlayerIdx] -= smallBlindAmount
-    bets[smallBlindPlayerIdx] += smallBlindAmount
-    stacks[bigBlindPlayerIdx] -= bigBlindAmount
-    bets[bigBlindPlayerIdx] += bigBlindAmount
+    # Set bets
+    players = setBet(players, getSmallBlindAmount(board), smallBlindPlayerIdx)
+    players = setBet(players, getBigBlindAmount(board), bigBlindPlayerIdx)
+    
+#    stacks[smallBlindPlayerIdx] -= smallBlindAmount
+#    bets[smallBlindPlayerIdx] += smallBlindAmount
+#    stacks[bigBlindPlayerIdx] -= bigBlindAmount
+#    bets[bigBlindPlayerIdx] += bigBlindAmount
     
     # Small blind acts first on the preflop in heads up
-    actingPlayerIdx = smallBlindPlayerIdx
+#    actingPlayerIdx = smallBlindPlayerIdx
     
     # Available actions
-    callAmount = smallBlindAmount
-    maxRaiseAmount = np.min(stacks) # + callAmount
+    availableActions = np.zeros(4, dtype=np.int)
+    availableActions[0]
+    
+    bets = getBets(players)
+    stacks = getStacks(players)
+    bigBlindAmount = getBigBlindAmount(board)
+    callAmount = np.abs(bets[0]-bets[1])
+    maxRaiseAmount = np.min(stacks)
+    raiseMinMax = np.array([min(max(callAmount+bigBlindAmount,callAmount*2), maxRaiseAmount), 
+                            maxRaiseAmount])   # Max raise is all-in
+    if(maxRaiseAmount < callAmount): raiseMinMax[:] = -1
+    
+    
+    callAmount = getSmallBlindAmount(board)
+    maxRaiseAmount = np.min(getStacks(players))
     raiseMinMax = np.array([min(callAmount+bigBlindAmount, maxRaiseAmount), maxRaiseAmount])   # Max raise is all-in
     if(maxRaiseAmount == callAmount): raiseMinMax[:] = -1
     
@@ -68,7 +120,26 @@ def initGame(blindPositions, smallBlindAmount, stacks):
         bettingRound, boardCardsVisible, actingPlayerIdx, callAmount, raiseMinMax
 
 
+
+
 # %%
+
+# "Static" variables (over single round)
+tmpCards = np.random.choice(52, size=9, replace=0)
+boardCards = tmpCards[:5]
+holeCards = np.array([tmpCards[5:7],tmpCards[7:]])
+smallBlindPlayerIdx = 1     # Which player is small blind?
+#blindPositions = np.random.choice(2, size=2, replace=0)     # 1st index = small blind player
+smallBlindAmount = 4
+
+# "Nonstatic" variables, these are modified by the engine
+stacks = np.array([smallBlindAmount*8+2,smallBlindAmount*6+5])
+
+
+
+# %%
+
+
 
 pot, bets, stacks, bigBlindAmount, numPlayersActed, gameEndState, bettingRound, \
     boardCardsVisible, actingPlayerIdx, callAmount, raiseMinMax = initGame(blindPositions, 
@@ -82,9 +153,8 @@ pot, bets, stacks, bigBlindAmount, numPlayersActed, gameEndState, bettingRound, 
 #   3rd is raise amount. 
 # Only one action can be declared, for instance, [-1,-1, 25] means raise to 25.
 
-action = np.array([-1,-1,raiseMinMax[1]])
-
-
+#action = np.array([-1,-1,raiseMinMax[1]])
+action = np.array([-1,callAmount,-1])
 
 
 pot, bets, stacks, bigBlindAmount, numPlayersActed, gameEndState, bettingRound, \
@@ -182,7 +252,7 @@ def executeAction(pot, bets, stacks, bigBlindAmount, numPlayersActed, gameEndSta
         maxRaiseAmount = np.min(stacks) #+ callAmount
         raiseMinMax = np.array([min(max(callAmount+bigBlindAmount,callAmount*2), maxRaiseAmount), 
                                 maxRaiseAmount])   # Max raise is all-in
-        if(maxRaiseAmount == callAmount): raiseMinMax[:] = -1
+        if(maxRaiseAmount < callAmount): raiseMinMax[:] = -1
 
 
         return pot, bets, stacks, bigBlindAmount, numPlayersActed, gameEndState, \
