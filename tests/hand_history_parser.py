2e4c40eeedb241db973f41c7415783937f9a4019
juldaani <juho.uusiluomalahti@gmail.com>
2019-02-03 18:10:34 +0200

    devel

--- tests/hand_history_parser.py
+++ tests/hand_history_parser.py
@@ -7,7 +7,7 @@ Created on Sat Feb  2 13:47:38 2019
 """
 
 
-PATH = '/home/juho/dev_folder/texas_hu_engine/tests/hand_histories/10/abs NLH handhq_1-OBFUSCATED.txt'
+PATH = '/home/juho/dev_folder/texas_hu_engine/tests/hand_histories/test_data7.txt'
 
 
 with open (PATH, "r") as myfile: data = myfile.read()
@@ -18,6 +18,21 @@ games = data.split('Stage')
 
 # %%
 
+def getReturnAmount(lines, lineIdx, playerId):
+    returnAmount = 0
+    
+    if( (len(lines)-1 >= lineIdx+2) and ('returned' in lines[lineIdx+2]) and not 
+        ('Folds' in lines[lineIdx+1]) ):
+        tmpLine = lines[lineIdx+2]
+        tmpSplit = tmpLine.split(' - ')
+        tmpPlayerId = tmpSplit[0]
+        assert playerId == tmpPlayerId
+        returnAmount = tmpSplit[1].split(' ($')[1].split(')')[0]
+        returnAmount = int(float(returnAmount) * multiplier)
+    
+    return returnAmount
+
+
 c = 0
 
 games2 = []
@@ -26,10 +41,6 @@ for game in games:
 
     # Only heads up no-limit games & not ante
     if( ('(1 on 1)' in game) & ('No Limit' in game) & ('ante' not in game) ):     
-        
-        c += 1
-        if(c > 1): break
-    
         # Multiply all amounts to remove decimal parts
         multiplier = 1000
         
@@ -42,6 +53,10 @@ for game in games:
                                             replace(',','') ) * multiplier)
         player2Stack = int(float(game.split(player2Id)[1].split('($')[1].split(' in ')[0].
                                             replace(',','') ) * multiplier)
+        
+        print('*** STACKS ***')
+        print(player1Id, player1Stack)
+        print(player2Id, player2Stack)
     
         # Player 1 blind amount & is small blind
         sttr = game.split(player1Id)[2]
@@ -56,6 +71,9 @@ for game in games:
         sttr = sttr[:idx]
         player2BlindAmount = int(float(sttr.split('$')[1]) * multiplier)
         
+        print('*** BLINDS ***')
+        print(player1Id, player1BlindAmount)
+        print(player2Id, player2BlindAmount)
         
         idx1 = game.find('*** POCKET CARDS ***')
         statesData = game[idx1:]
@@ -66,35 +84,70 @@ for game in games:
         gameStatesDict = {'pocket_cards':None, 'flop':None, 'turn':None, 'river':None, 
                           'show_down':None, 'summary':None}
         
+
+
         for stateStr, key in zip(gameStateSearchStrings, gameStatesDict):
             
-            print(stateStr, key)
+            print(stateStr)
             
             if(stateStr in statesData):
-                
-                assert 0
-                
                 
                 sttr = statesData.split(stateStr)[1]
                 sttr = sttr[:sttr.find('***')]
                 lines = sttr.splitlines()[1:]
                 
-                for line in lines:
-                    split = line.split(' - ')
-                    playerId = split[0]
-                    action, amount = split[1].split(' $')
-                    amount = int(float(amount) * multiplier)
+                for lineIdx in range(len(lines)):
+                    line = lines[lineIdx]                    
                     
-                
-                gameStatesDict[key] = asdfad
+                    if(key == 'pocket_cards' or key == 'flop' or key == 'turn' or key == 'river'):
+                        split = line.split(' - ')
+                        playerId = split[0]
+
+                        if('Calls' in split[1]):
+                            action, amount = split[1].split(' $')
+                            amount = int(float(amount) * multiplier)
+                            print(playerId, action,amount)
+                        elif('Bets' in split[1]):
+                            action, amount = split[1].split(' $')
+                            amount = int(float(amount) * multiplier)
+                            returnAmount = getReturnAmount(lines, lineIdx, playerId)
+                            amount -= returnAmount
+                            
+#                            if(key == 'river'):
+#                                assert 0
+                            
+                            print(playerId, action,amount)
+                        elif('Raises' in split[1]):
+                            action, amount, _ = split[1].split(' $')
+                            amount = int(float(amount.split(' to')[0]) * multiplier)
+                            returnAmount = getReturnAmount(lines, lineIdx, playerId)
+                            amount -= returnAmount     
+                            print(playerId, action,amount)
+                        elif('Checks' in split[1]):
+                            action, amount = 'Checks', 0
+                            print(playerId, action,amount)
+                        elif('Folds' in split[1]):
+                            action, amount = 'Folds', 0
+                            print(playerId, action,amount)
+                        elif('All-In(Raise)' in split[1]):
+                            _, amount, _ = split[1].split(' $')
+                            action = 'All-In'
+                            amount = int(float(amount.split(' to')[0]) * multiplier)
+                            returnAmount = getReturnAmount(lines, lineIdx, playerId)
+                            amount -= returnAmount     
+                            print(playerId, action,amount)          
+                        elif('All-In' in split[1]):
+                            action, amount = split[1].split(' $')
+                            amount = int(float(amount.replace(',','')) * multiplier)
+                            returnAmount = getReturnAmount(lines, lineIdx, playerId)
+                            amount -= returnAmount     
+                            print(playerId, action,amount)
+                            
+
+                    
+#                gameStatesDict[key] = asdfad
                                         
         
-        
-        [state in sttr for state in gameStates]
-        
-        sttr.split('*#* AAD *')
-        
-        sttr.splitlines()
 
 
 
